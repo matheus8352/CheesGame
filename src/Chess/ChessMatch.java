31c59b142348bbfbc5c14745beeb80fed371776d
Matheus Alexandre <matheus8352@gmail.com>
2020-02-16 18:44:17 -0300

    Classes UI, ChessMatch, ChessPiece and Enum Color created

---
+++ src/Chess/ChessMatch.java
@@ -0,0 +1,20 @@
+package Chess;
+
+import BoardGame.Board;
+
+public class ChessMatch {
+
+	private Board board;
+	
+	public ChessMatch() {
+		board = new Board(8,8);
+	}
+	
+	public ChessPiece[][] getPieces(){
+		ChessPiece[][] aux = new ChessPiece[board.getRows()][board.getColumns()];
+		for(int i = 0; i < board.getRows(); i++)
+			for (int j = 0; j < board.getColumns(); j++) 
+				aux[i][j] = (ChessPiece) board.getPiece(i, j);
+		return aux;
+	}
+}
