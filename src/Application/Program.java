1a8a724a469add1d0d87636c97c34a9b5e926fc2
Matheus Alexandre <matheus8352@gmail.com>
2020-02-24 17:16:47 -0300

    Placing pieces on the board

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,9 +1,12 @@
 package Application;
 
+import Chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-
+		ChessMatch match = new ChessMatch();
+		UI.printBoard(match.getPieces());
 	}
 
 }
